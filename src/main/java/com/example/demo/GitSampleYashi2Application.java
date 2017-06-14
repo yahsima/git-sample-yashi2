e7f8236f27f8618a7e84915e759bba6418b0adeb
hideyukiyashima <hideyukiyashima@rakus.co.jp>
2017-06-14 11:11:09 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/demo/GitSampleYashi2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleYashi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleYashi2Application.class, args);
+	}
+}
